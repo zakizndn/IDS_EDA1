76c04dfc61155e4ec8ce65e22f90b8e122d665b3
Zaki <117178074+zakizndn@users.noreply.github.com>
2024-01-15 08:09:04 +0800

    Update eda1.py

--- eda1.py
+++ eda1.py
@@ -1,35 +1,26 @@
 import pandas as pd
 import seaborn as sns
+import streamlit as st
+import matplotlib.pyplot as plt
 
+# Load the data
 mydf = pd.read_csv('mall_customer.csv')
 mydf.rename(columns={'Genre': 'Gender'}, inplace=True)
-mydf.sample(n = 10)
 
-mydf.describe()
+# Display a sample of the data
+st.write("#### Sample of the Data")
+st.write(mydf.sample(n=10))
 
-#!pip install sweetviz
+# Display descriptive statistics
+st.write("#### Descriptive Statistics")
+st.write(mydf.describe())
 
-# Importing sweetviz library
-#import sweetviz as sv
+# Display a scatter plot
+st.write("#### Scatter Plot: Annual Income vs Spending Score")
+scatter_plot = sns.regplot(data=mydf, x='Annual_Income_(k$)', y='Spending_Score')
+st.pyplot()
 
-# Analyzing the dataset
-#advert_report = sv.analyze(mydf)
-
-# Display the report
-#advert_report.show_html('Advertising.html')
-
-#advert_report.show_notebook()
-
-sns.regplot(data = mydf, x = 'Annual_Income_(k$)', y = 'Spending_Score');
-
-sns.boxplot(data = mydf, x = 'Gender', y = 'Annual_Income_(k$)');
-
-#corr = mydf.corr(method = 'pearson')
-#corr
-
-#import matplotlib.pyplot as plt
-#%matplotlib inline
-
-#plt.figure(figsize=(10,8), dpi =500)
-#sns.heatmap(corr,annot=True,fmt=".2f", linewidth=.5)
-#plt.show()
+# Display a box plot
+st.write("#### Box Plot: Annual Income by Gender")
+box_plot = sns.boxplot(data=mydf, x='Gender', y='Annual_Income_(k$)')
+st.pyplot()
